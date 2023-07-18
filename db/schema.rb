477ece01026ebffc9393dd8bd0c38620365ce8e2
Pankaj Bagdare <pankajbagdare@gmail.com>
2023-07-18 18:39:16 +0530

    - Add the name column to the users table. - Include name attribute to sign_up_params and account_update_params   methods inside the RegistrationsController

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,9 @@ ActiveRecord::Schema.define(version: 2023_07_18_123445) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "user_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
