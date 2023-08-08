c331977c390ea9de9144b19f22a6b0ae648109bb
Pankaj Bagdare <pankajbagdare@gmail.com>
2023-08-08 11:26:10 +0530

    Add specs to NavigationHelper's collapsible_links_partial_path method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,7 +39,11 @@ RSpec.configure do |config|
   # instead of true.
   # config.use_transactional_fixtures = true
   config.use_transactional_fixtures = false
-  
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include FactoryBot::Syntax::Methods
+
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
